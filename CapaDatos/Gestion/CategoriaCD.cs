7c374e89807d11cd148dc874186733b806d30e24
jch-berrezueta <jcberrezueta8@gmail.com>
2025-02-13 01:00:31 -0500

    ok

--- CapaDatos/Gestion/CategoriaCD.cs
+++ CapaDatos/Gestion/CategoriaCD.cs
@@ -1,12 +1,91 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+using Categoria = CapaEntidades.Gestion.Categoria;
 
 namespace CapaDatos.Gestion
 {
     public class CategoriaCD
     {
+        public static List<CP_ListarCategoriasProductosResult> listarCategoriasCD()
+        {
+            ConectorBDDataContext bd = null;
+            List<CP_ListarCategoriasProductosResult> lista = null;
+            try
+            {
+                bd = new ConectorBDDataContext();
+                lista = bd.CP_ListarCategoriasProductos().ToList();
+                bd.SubmitChanges();
+            }
+            catch(Exception error)
+            {
+                Debug.WriteLine("Error en listar Categorias CD" + error);
+            }
+            return lista;
+        }
+
+        public static List<CP_FiltrarCategoriasProductosResult> filtrarCategoriasCD(string clave)
+        {
+            ConectorBDDataContext bd = null;
+            List<CP_FiltrarCategoriasProductosResult> lista = null;
+            try
+            {
+                bd = new ConectorBDDataContext();
+                lista = bd.CP_FiltrarCategoriasProductos(clave).ToList();
+                bd.SubmitChanges();
+            }
+            catch (Exception error)
+            {
+                Debug.WriteLine("Error en filtrar Categorias CD" + error);
+            }
+            return lista;
+        }
+
+        public static void insertarCategoriaCD(Categoria Categoria)
+        {
+            ConectorBDDataContext bd = null;
+            try
+            {
+                bd = new ConectorBDDataContext();
+                bd.CP_InsertarCategoriaProducto(Categoria.Nombre, Categoria.Descripcion);
+                bd.SubmitChanges();
+            }
+            catch (Exception error)
+            {
+                Debug.WriteLine("Error en insertar Categorias CD" + error);
+            }
+        }
+        public static void modificarCategoriaCD(Categoria Categoria)
+        {
+            ConectorBDDataContext bd = null;
+            try
+            {
+                bd = new ConectorBDDataContext();
+                bd.CP_ModificarCategoriaProducto(Categoria.IdCategoria, Categoria.Nombre, Categoria.Descripcion);
+                bd.SubmitChanges();
+            }
+            catch (Exception error)
+            {
+                Debug.WriteLine("Error en modificar Categorias CD" + error);
+            }
+        }
+        public static void eliminarCategoriaCD(int idCategoria)
+        {
+            ConectorBDDataContext bd = null;
+            try
+            {
+                bd = new ConectorBDDataContext();
+                bd.CP_EliminarCategoriaProducto(idCategoria);
+                bd.SubmitChanges();
+            }
+            catch (Exception error)
+            {
+                Debug.WriteLine("Error en eliminar Categorias CD" + error);
+            }
+        }
     }
 }
--- CapaDatos/Gestion/ClienteCD.cs
+++ CapaDatos/Gestion/ClienteCD.cs
@@ -1,12 +1,93 @@
-﻿using System;
+﻿using CapaEntidades.Gestion;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+using Cliente = CapaEntidades.Gestion.Cliente;
 
 namespace CapaDatos.Gestion
 {
     public class ClienteCD
     {
+        public static List<CP_ListarClientesResult> listarClientesCD()
+        {
+            ConectorBDDataContext bd = null;
+            List<CP_ListarClientesResult> lista = null;
+            try
+            {
+                bd = new ConectorBDDataContext();
+                lista = bd.CP_ListarClientes().ToList();
+                bd.SubmitChanges();
+            }
+            catch (Exception error)
+            {
+                Debug.WriteLine("Error en listar Clientes CD" + error);
+            }
+            return lista;
+        }
+
+        public static List<CP_FiltrarClientesResult> filtrarClientesCD(string clave)
+        {
+            ConectorBDDataContext bd = null;
+            List<CP_FiltrarClientesResult> lista = null;
+            try
+            {
+                bd = new ConectorBDDataContext();
+                lista = bd.CP_FiltrarClientes(clave).ToList();
+                bd.SubmitChanges();
+            }
+            catch (Exception error)
+            {
+                Debug.WriteLine("Error en filtrar Clientes CD" + error);
+            }
+            return lista;
+        }
+
+        public static void insertarClienteCD(Cliente Cliente)
+        {
+            ConectorBDDataContext bd = null;
+            try
+            {
+                bd = new ConectorBDDataContext();
+                bd.CP_InsertarCliente(Cliente.Id_Cuenta, Cliente.Nombre, Cliente.Apellido, Cliente.FechaNacimiento, Cliente.Edad, Cliente.Telefono);
+                bd.SubmitChanges();
+            }
+            catch (Exception error)
+            {
+                Debug.WriteLine("Error en insertar Clientes CD" + error);
+            }
+        }
+        public static void modificarClienteCD(Cliente Cliente)
+        {
+            ConectorBDDataContext bd = null;
+            try
+            {
+                bd = new ConectorBDDataContext();
+                bd.CP_ModificarCliente(Cliente.Id_Cliente, Cliente.Id_Cuenta, Cliente.Nombre, Cliente.Apellido, Cliente.FechaNacimiento, Cliente.Edad, Cliente.Telefono);
+                bd.SubmitChanges();
+            }
+            catch (Exception error)
+            {
+                Debug.WriteLine("Error en modificar Clientes CD" + error);
+            }
+        }
+        public static void eliminarClienteCD(int idCliente)
+        {
+            ConectorBDDataContext bd = null;
+            try
+            {
+                bd = new ConectorBDDataContext();
+                bd.CP_EliminarCliente(idCliente);
+                bd.SubmitChanges();
+            }
+            catch (Exception error)
+            {
+                Debug.WriteLine("Error en eliminar Clientes CD" + error);
+            }
+        }
+
     }
 }
