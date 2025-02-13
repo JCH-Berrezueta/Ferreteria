00f6b84e40dce30fbc5ecbd40765608e3b2f2121
Jake24012001 <oberinho2401@outlook.com>
2025-02-13 17:56:16 -0500

    Capa Api-Creacion de clase Cliente

--- CapaAPI/Controllers/Cliente.cs
+++ CapaAPI/Controllers/Cliente.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Web.Http;
+using ClienteLN = CapaLogica.Gestion.ClienteLN;
 namespace CapaAPI.Controllers
 {
-    public class Cliente
+    [RoutePrefix("api/cliente")]
+    public class Cliente : ApiController
     {
-
+        public Cliente()
+        {
+        }
+        //[HttpGet]
+        //[Route("obtener-clientes")]
+        //public IHttpActionResult ObtenerClientes()
+        //{
+        //    // Aquí estamos llamando a la capa lógica para obtener los clientes
+        //    //var clientes = ClienteLN.listarClientesLN();
+        //    //return Ok(clientes);  // Retornamos la lista de clientes
+        //}
     }
 }
