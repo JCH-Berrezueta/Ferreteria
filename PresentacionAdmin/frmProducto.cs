2e895549321d580eea34af3cfd19e602f75da50b
Jake24012001 <oberinho2401@outlook.com>
2025-02-13 19:53:26 -0500

    jk

--- PresentacionAdmin/frmProducto.cs
+++ PresentacionAdmin/frmProducto.cs
@@ -17,6 +17,7 @@ namespace PresentacionAdmin
         public frmProducto()
         {
             InitializeComponent();
+            CargarProductos();
         }
 
         private void frmProducto_Load(object sender, EventArgs e)
@@ -32,7 +33,26 @@ namespace PresentacionAdmin
 
         private void button1_Click(object sender, EventArgs e)
         {
-           
+            Producto nuevoProducto = new Producto
+            {
+                Id_CategoriaProducto = 1, // Debes asignar la categoría correspondiente
+                Nombre = "Nuevo Producto",
+                Precio = 10.0m,
+                Stock = 100,
+                Estado = true,
+                Icono = null, // Puedes asignar una imagen si es necesario
+                Descripcion = "Descripción del producto"
+            };
+
+            if (ProductoLN.listarProductosLN(nuevoProducto))
+            {
+                MessageBox.Show("Producto agregado correctamente");
+                CargarProductos();
+            }
+            else
+            {
+                MessageBox.Show("Error al agregar producto");
+            }
         }
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
