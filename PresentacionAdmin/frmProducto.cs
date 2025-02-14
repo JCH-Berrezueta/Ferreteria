2d17286f9e0532c1be85c12d4f93b78583ba0a6e
Jake24012001 <oberinho2401@outlook.com>
2025-02-14 00:59:06 -0500

    productoln

--- PresentacionAdmin/frmProducto.cs
+++ PresentacionAdmin/frmProducto.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using CapaEntidades.Gestion;
+
 using CapaLogica.Gestion;
 namespace PresentacionAdmin
 {
@@ -34,6 +34,7 @@ namespace PresentacionAdmin
 
         private void button1_Click(object sender, EventArgs e)
         {
+<<<<<<< HEAD
             frmEditProducto frm = new frmEditProducto();
             frm.ShowDialog();
             if(frm.DialogResult == DialogResult.OK)
@@ -44,6 +45,9 @@ namespace PresentacionAdmin
                 ProductoLN.listarProductosLN();
 
             }
+=======
+           
+>>>>>>> productoln
         }
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
@@ -52,58 +56,12 @@ namespace PresentacionAdmin
 
         private void button2_Click(object sender, EventArgs e)
         {
-            /*if (dataGridView1.SelectedRows.Count > 0)
-            {
-                int idProducto = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["Id_Producto"].Value);
-                Producto productoModificado = new Producto
-                {
-                    Id_Producto = idProducto,
-                    Id_CategoriaProducto = 1,
-                    Nombre = "Producto Modificado",
-                    Precio = 20.0m,
-                    Stock = 50,
-                    Estado = true,
-                    Icono = null,
-                    Descripcion = "Nueva descripción"
-                };
-
-                if (ProductoLN.ModificarProductoLN(productoModificado))
-                {
-                    MessageBox.Show("Producto modificado correctamente");
-                    CargarProductos();
-                }
-                else
-                {
-                    MessageBox.Show("Error al modificar producto");
-                }
-            }
-            else
-            {
-                MessageBox.Show("Seleccione un producto para modificar");
-            }*/
 
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            /*if (dataGridView1.SelectedRows.Count > 0)
-            {
-                int idProducto = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["Id_Producto"].Value);
-
-                if (ProductoLN.EliminarProductoLN(idProducto))
-                {
-                    MessageBox.Show("Producto eliminado correctamente");
-                    CargarProductos();
-                }
-                else
-                {
-                    MessageBox.Show("Error al eliminar producto");
-                }
-            }
-            else
-            {
-                MessageBox.Show("Seleccione un producto para eliminar");
-            }*/
+           
         }
     }
 
