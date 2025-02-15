b5d84800e093cd61aa09eee5cfe225ad647bf6dc
Jake24012001 <oberinho2401@outlook.com>
2025-02-14 23:14:53 -0500

    .

---
+++ PresentacionAdmin/Gestion/frmCategoriaProducto.cs
@@ -0,0 +1,52 @@
+﻿using CapaEntidades.Gestion;
+using CapaLogica.Gestion;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PresentacionAdmin
+{
+    public partial class frmCategoriaProducto : Form
+    {
+        public frmCategoriaProducto()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*Producto nuevoProducto = new Producto()
+            
+            Id_CategoriaProducto = Convert.ToInt32(txtCategoria.Text);
+            Nombre = txtNombre.Text;
+            Precio = Convert.ToDecimal(txtPrecio.Text);
+            Stock = Convert.ToInt32(txtStock.Text);
+            Estado = chkEstado.Checked;
+            Icono = "ruta/icono.png"; // Puedes modificar esto para seleccionar un icono
+            Descripcion = txtDescripcion.Text;
+            
+
+            if (ProductoLN.agregarProductoLN(nuevoProducto))
+            {
+                MessageBox.Show("Producto agregado correctamente", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                CargarProductos(); // Actualiza la lista en el DataGridView
+            }
+            else
+            {
+                MessageBox.Show("Error al agregar el producto", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }*/
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
